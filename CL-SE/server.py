6563d9f1463adb8dfaaa372a4ee0dd75f49cc927
AlexNdungu <alexmeta517@gmail.com>
2023-09-15 11:41:00 +0300

    Client And Server

--- CL-SE/server.py
+++ CL-SE/server.py
@@ -3,23 +3,39 @@ import threading
 
 # Server configuration
 
+HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 # Create socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+# Handle client connection
 def handle_client(conn, addr):
-    pass
+    
+    print(f"[NEW CONNECTION] {addr} connected.")
 
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f"[{addr}] {msg}")
+
+
+# Start server
 def start():
     server.listen()
 
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
         
 
 
